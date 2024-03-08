6a8a7e65ce4e32908a0ffb72a9f2c2c409503656
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2024-03-08 10:29:02 -0300

    FEAT: security filter

---
+++ src/main/java/max/gateway/security/RouterValidator.java
@@ -0,0 +1,30 @@
+package max.gateway.security;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouterValidator {
+    
+    //@Value("${api.endpoints.open}")
+    private List<String> openApiEndpoints = List.of(
+        "POST /auth/register",
+        "POST /auth/login",
+        "ANY /auth/register"
+    );
+
+    public final Predicate<ServerHttpRequest> isSecured = 
+        request -> openApiEndpoints
+            .stream()
+            .noneMatch(uri -> {
+                String[] parts = uri.replaceAll("[^a-zA-Z0-9// ]", "").split(" ");
+                final String method = parts[0];
+                final String path = parts[1];
+                
+                return (request.getMethod().toString().equalsIgnoreCase(method) || method.equalsIgnoreCase("ANY")) 
+                    && request.getURI().getPath().equals(path);
+            });
+}
